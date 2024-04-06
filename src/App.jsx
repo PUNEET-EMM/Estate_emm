9a1bb3045a05eb3063c9395ff2c188bd9e920892
Puneet <puneet19338@gmail.com>
2024-04-06 13:33:48 +0530

    ListPage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,54 @@
-import { useState } from 'react'
-import Navbar from './components/navbar/Navbar'
-import "./layout.scss"
 
-import './App.css'
-import HomePage from './routes/homePage/homePage'
+import HomePage from "./routes/homePage/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+// import SinglePage from "./routes/singlePage/singlePage";
+// import ProfilePage from "./routes/profilePage/profilePage";
+// import Login from "./routes/login/login";
+// import Register from "./routes/register/register";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        // {
+        //   path:"/:id",
+        //   element:<SinglePage/>
+        // },
+        // {
+        //   path:"/profile",
+        //   element:<ProfilePage/>
+        // },
+        // {
+        //   path:"/login",
+        //   element:<Login/>
+        // },
+        // {
+        //   path:"/register",
+        //   element:<Register/>
+        // }
+      ]
+    }
+  ]);
 
   return (
-    <div className="layout">
-    <div className="navbar">
-      <Navbar />
-    </div>
-    <div className="content">
-      <HomePage/>
-    </div>
-  </div>
-  )
+
+    <RouterProvider router={router}/>
+  );
 }
 
-export default App
+export default App;
