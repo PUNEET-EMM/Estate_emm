07608f54f344d8b6834998aae4a8796fcc1009b7
Puneet <puneet19338@gmail.com>
2024-04-10 22:31:37 +0530

    Prduct page added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
-// import SinglePage from "./routes/singlePage/singlePage";
+import SinglePage from "./routes/singlePage/singlePage";
 // import ProfilePage from "./routes/profilePage/profilePage";
 // import Login from "./routes/login/login";
 // import Register from "./routes/register/register";
@@ -25,10 +25,10 @@ function App() {
           path:"/list",
           element:<ListPage/>
         },
-        // {
-        //   path:"/:id",
-        //   element:<SinglePage/>
-        // },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
         // {
         //   path:"/profile",
         //   element:<ProfilePage/>
