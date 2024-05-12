1dcf1412a70e5da87d8bd166fb0bffa95a4073fa
Puneet <puneet19338@gmail.com>
2024-05-12 11:55:23 +0530

    deploy

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+import { useContext, useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import { useNotificationStore } from "../../lib/notificationStore";
-
 function Navbar() {
   const [open, setOpen] = useState(false);
 
--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://estate-server-oxyg.onrender.com/api",
   withCredentials: true,
 });
 
