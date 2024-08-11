a80c28fcf3256015ba4ed9effaacf2f554dbc40a
Puneet <puneet19338@gmail.com>
2024-08-11 19:52:49 +0530

    production

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// const apiRequest = axios.create({
-//   baseURL: "https://estate-server-oxyg.onrender.com/api",
-//   withCredentials: true,
-// });
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://estate-server-oxyg.onrender.com/api",
   withCredentials: true,
 });
+// const apiRequest = axios.create({
+//   baseURL: "http://localhost:8800/api",
+//   withCredentials: true,
+// });
 
 export default apiRequest;
