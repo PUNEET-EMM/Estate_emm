0b8cd88c63254d48bcee1f6559c52c163e2f7156
Puneet <puneet19338@gmail.com>
2025-06-22 22:11:49 +0530

    Fix

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -9,7 +9,9 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io(import.meta.env.VITE_SOCKET_URL));
+    setSocket(io(import.meta.env.VITE_SOCKET_URL, {
+  withCredentials: true,
+}));
   }, []);
 
   useEffect(() => {
